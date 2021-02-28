4319b0f51709dc9414f9f38200d5c589f146092c
Michael Baschenko <m.baschenko12@gmail.com>
2021-02-28 17:58:44 +0300

    Задание 4

--- Lab1/lab1.2.rb
+++ Lab1/lab1.2.rb
@@ -8,3 +8,9 @@ unless lang == 'ruby'
 else
 	puts 'Подлиза.'
 end
+print "Введите команду Ruby: "
+com_ruby = gets.chomp
+print "Введите команду ОС: "
+com_os = gets.chomp
+system("ruby -e \"#{com_ruby}\"")
+system("#{com_os}")
