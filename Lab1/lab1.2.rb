26d2bbac7c2ce8584123079b257611e839c402fe
Michael Baschenko <m.baschenko12@gmail.com>
2021-02-28 17:55:44 +0300

    Задание 3 с использованием unless-else

--- Lab1/lab1.2.rb
+++ Lab1/lab1.2.rb
@@ -3,10 +3,8 @@ puts "Hello #{name}"
 ARGV.clear
 print 'Какой язык у Вас самый любимый? '
 lang = gets.chomp
-if lang == 'ruby'
+unless lang == 'ruby'
+	puts 'Скоро будет ruby.'
+else
 	puts 'Подлиза.'
-elsif lang == 'python'
-	puts 'Скоро будет ruby. Вкус у Вас, конечно, не очень.'
-elsif lang == 'java'
-	puts 'Скоро будет ruby, но выбор хороший.'
 end
