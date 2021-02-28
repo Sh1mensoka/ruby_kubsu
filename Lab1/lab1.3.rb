86f8b6e6681a28b25008dc248fcf5caffc7246cb
Michael Baschenko <m.baschenko12@gmail.com>
2021-02-28 18:02:06 +0300

    Задание 6

--- Lab1/lab1.3.rb
+++ Lab1/lab1.3.rb
@@ -1,7 +1,42 @@
 input_num = ARGV[0].to_i
-sum = 0
-while input_num > 0
-	sum += input_num.remainder(10)
-	input_num /=  10
+
+def num_sum(input)
+	sum, num = 0, input
+	while num > 0
+		sum += num.remainder(10)
+		num /=  10
+	end
+	puts "Сумма цифр числа: #{sum}"
 end
-puts sum
+
+def search_min(input)
+	min, num = input.remainder(10), input / 10
+	while num > 0
+		min = num.remainder(10) if num.remainder(10) < min
+		num /= 10
+	end
+	puts "Наименьшая цифра числа: #{min}"
+end
+
+def search_max(input)
+	max, num = input.remainder(10), input / 10
+	while num > 0
+		max = num.remainder(10) if num.remainder(10) > max
+		num /= 10
+	end
+	puts "Наибольшая цифра числа: #{max}"
+end
+
+def num_multiplication(input)
+	res, num = 1, input
+	while num > 0
+		res *= num.remainder(10)
+		num /= 10
+	end
+	puts "Произведение цифр числа: #{res}"
+end
+
+num_sum(input_num)
+search_min(input_num)
+search_max(input_num)
+num_multiplication(input_num)
