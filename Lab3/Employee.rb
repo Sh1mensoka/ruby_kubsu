4e8693a01a5f47dfb2084007e02bace1a62f2b45
Michael Baschenko <m.baschenko12@gmail.com>
2021-04-10 11:40:56 +0300

    Задания 5-7

--- Lab3/Employee.rb
+++ Lab3/Employee.rb
@@ -58,7 +58,7 @@ class Employee
 	end
 
 	def year=(year)
-		@year = year
+		@year = Employee.year_convert(year)
 	end
 
 	def tel=(tel)
@@ -74,7 +74,7 @@ class Employee
 	end
 
 	def pass=(pass)
-		@pass = pass
+		@pass = Employee.pass_convert(pass)
 	end
 
 	def spec=(spec)
@@ -119,7 +119,7 @@ class Employee
 		end
 	end
 
-	def self.check_email?(email)
+	def self.email_check?(email)
 		if email =~ /^[\w]*+@[\w]*+\.+[A-Za-z]{2,3}$/
 			true
 		else
@@ -128,11 +128,95 @@ class Employee
 	end
 
 	def self.email_convert(email)
-		if !self.check_email?(email)
+		if !self.email_check?(email)
 			raise "Некорректный адрес электронной почты!" 
 		else
 			email.downcase
 		end
 	end
 
+	def self.fio_check?(fio)
+		if fio =~ /^ *[А-Яа-я]* *-? *[А-Яа-я]* *[А-Яа-я]* *-? *[А-Яа-я]* *[А-Яа-я]* *[А-Яа-я]* */
+			true
+		else
+			false
+		end
+	end
+
+	def self.fio_convert(fio)
+		if !self.fio_check?(fio)
+			raise "Некорректные имя фамилия отчество!"
+		else
+			# TODO
+		end
+	end
+
+
+	def self.year_check?(year)
+		if year =~ /^\d{1,2}.\d{1,2}.\d{2,4}/
+			temp = year.split(".")
+			case temp[1].to_i
+			when 1, 3, 5, 7, 8, 10, 12
+				if temp[0].to_i > 0 && temp[0].to_i < 32
+					true
+				else
+					false
+				end
+			when 2
+				if temp[0].to_i > 0 && temp[0].to_i < 29
+					true
+				else
+					false
+				end	
+			when 4, 6, 9, 11
+				if temp[0].to_i > 0 && temp[0].to_i < 31
+					true
+				else
+					false
+				end	
+			else
+				false
+			end
+		else
+			false
+		end
+	end
+
+	def self.year_convert(year)
+		if !self.year_check?(year)
+			raise "Некорректный год!"
+		else
+			temp = year.split(".")
+			if temp[0].length < 2
+				temp[0].insert(0, "0")
+			end
+			if temp[1].length < 2
+				temp[1].insert(0, "0")
+			end
+			new_year = "#{temp[0]}.#{temp[1]}.#{temp[2]}"
+		end
+	end
+
+	def self.pass_check?(pass)
+		if pass =~ /^\d{4} ?\d{6}/
+			true
+		else
+			false
+		end
+	end
+
+	def self.pass_convert(pass)
+		if !self.pass_check?(pass)
+			raise "Некорректные паспортные данные!"
+		else
+			if pass.split(" ").length < 2
+				pass.insert(4, " ")
+			end
+			pass
+		end
+	end
+
+	def self.test_class()
+
+	end
 end
